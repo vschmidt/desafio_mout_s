49b1797d3f8f815b0bc20ff0d79b37e83d07d2fe
vschmidt <schmidtvinicio@gmail.com>
2021-07-06 15:24:09 -0300

    Restruct service A

---
+++ service_a/handlers.py
@@ -0,0 +1,81 @@
+import tornado.web
+import json
+import os
+import pika
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    """
+    IndexHandler
+    """
+
+    def get(self):
+        """Return index page
+        ---
+        tags: [index]
+        summary: Return index page
+        description: Index page is UI interface with users
+        responses:
+            200:
+                description: Index page
+                content:
+                    application/html:
+                        schema:
+                            type: array
+        """
+        self.render("pages/index.html", title="Send interface")
+
+    def post(self):
+        id = self.get_argument("id", None)  # Get id
+        notification = self.get_argument("notification", None)  # Get notification
+        payload = {"id": id, "notification": notification}  # Create payload
+
+        # Get rabbitmq env variables
+        RABBITMQ_USER = os.getenv("RABBITMQ_USER", None)
+        RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", None)
+        RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", None)
+        RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", None)
+        RABBITMQ_VHOST = os.getenv("RABBITMQ_VHOST", None)
+        RABBITMQ_QUEUE = os.getenv("RABBITMQ_QUEUE", None)
+        RABBITMQ_EXCHANGE = os.getenv("RABBITMQ_EXCHANGE", None)
+
+        # Create connection
+        credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
+        parameters = pika.ConnectionParameters(
+            RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_VHOST, credentials
+        )
+        connection = pika.BlockingConnection(parameters)
+
+        channel = connection.channel()  # Open channel
+        channel.queue_declare(
+            queue=RABBITMQ_QUEUE,
+            durable=True,
+            auto_delete=False,
+            arguments={"x-max-length": 5, "x-queue-type": "classic"},
+        )
+
+        channel.basic_publish(
+            exchange=RABBITMQ_EXCHANGE,
+            routing_key="notifications",
+            body=json.dumps(payload),
+        )  # Publish message
+
+        connection.close()  # Close connection
+
+        self.render(
+            "pages/success.html", title="Success page", notification=notification
+        )
+
+
+class SearchHandler(tornado.web.RequestHandler):
+    def get(self):
+        # Get env variables
+        SERVICEB_HOST = os.getenv("SERVICEB_HOST", None)
+        SERVICEB_PORT = os.getenv("SERVICEB_PORT", None)
+
+        self.render(
+            "pages/search.html",
+            title="Search interface",
+            serviceb_host=SERVICEB_HOST,
+            serviceb_port=SERVICEB_PORT,
+        )
--- service_a/index.py
+++ service_a/index.py
@@ -1,115 +1,12 @@
 import tornado.ioloop
 import tornado.web
-import json
-import time
 import os
-import pika
 import swagger_ui
 from init_swagger import generate_swagger_file
+from utils import check_rabit
+from handlers import IndexHandler, SearchHandler
 
 SWAGGER_API_OUTPUT_FILE = "./docs/swagger_service_a.json"
-
-
-class IndexHandler(tornado.web.RequestHandler):
-    """
-    IndexHandler
-    """
-
-    def get(self):
-        """Return index page
-        ---
-        tags: [index]
-        summary: Return index page
-        description: Index page is UI interface with users
-        responses:
-            200:
-                description: Index page
-                content:
-                    application/html:
-                        schema:
-                            type: array
-        """
-        self.render("pages/index.html", title="Send interface")
-
-    def post(self):
-        id = self.get_argument("id", None)  # Get id
-        notification = self.get_argument("notification", None)  # Get notification
-        payload = {"id": id, "notification": notification}  # Create payload
-
-        # Get rabbitmq env variables
-        RABBITMQ_USER = os.getenv("RABBITMQ_USER", None)
-        RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", None)
-        RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", None)
-        RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", None)
-        RABBITMQ_VHOST = os.getenv("RABBITMQ_VHOST", None)
-        RABBITMQ_QUEUE = os.getenv("RABBITMQ_QUEUE", None)
-        RABBITMQ_EXCHANGE = os.getenv("RABBITMQ_EXCHANGE", None)
-
-        # Create connection
-        credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
-        parameters = pika.ConnectionParameters(
-            RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_VHOST, credentials
-        )
-        connection = pika.BlockingConnection(parameters)
-
-        channel = connection.channel()  # Open channel
-        channel.queue_declare(
-            queue=RABBITMQ_QUEUE,
-            durable=True,
-            auto_delete=False,
-            arguments={"x-max-length": 5, "x-queue-type": "classic"},
-        )
-
-        channel.basic_publish(
-            exchange=RABBITMQ_EXCHANGE,
-            routing_key="notifications",
-            body=json.dumps(payload),
-        )  # Publish message
-
-        connection.close()  # Close connection
-
-        self.render(
-            "pages/success.html", title="Success page", notification=notification
-        )
-
-
-class SearchHandler(tornado.web.RequestHandler):
-    def get(self):
-        # Get env variables
-        SERVICEB_HOST = os.getenv("SERVICEB_HOST", None)
-        SERVICEB_PORT = os.getenv("SERVICEB_PORT", None)
-
-        self.render(
-            "pages/search.html",
-            title="Search interface",
-            serviceb_host=SERVICEB_HOST,
-            serviceb_port=SERVICEB_PORT,
-        )
-
-
-def check_rabit():
-    """
-    Check if RabitMQ is ready
-    """
-    # Get rabbitmq env variables
-    RABBITMQ_USER = os.getenv("RABBITMQ_USER", None)
-    RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", None)
-    RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", None)
-    RABBITMQ_PORT = os.getenv("RABBITMQ_PORT", None)
-    RABBITMQ_VHOST = os.getenv("RABBITMQ_VHOST", None)
-
-    while True:
-        try:
-            credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASSWORD)
-            parameters = pika.ConnectionParameters(
-                RABBITMQ_HOST, RABBITMQ_PORT, RABBITMQ_VHOST, credentials
-            )
-            connection = pika.BlockingConnection(parameters)
-            connection.close()
-            break
-        except:
-            print(f"Connection with RabitMQ fail. Another try in 20 seconds")
-            time.sleep(20)
 
 
 def make_app():
